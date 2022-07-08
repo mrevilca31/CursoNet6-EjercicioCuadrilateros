dc4f607b8505160ce714627e7aefd10e93234961
Misael Vilca <mrevilca31@gmail.com>
2022-07-08 13:52:23 -0300

    Se añade clase Rectangulo y programa que instancia los objetos y calcula las areas

---
+++ EjercicioCuadrilateros/EjercicioCuadrilateros/Modelo/Rectangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCuadrilateros.Modelo
+{
+    internal class Rectangulo : Cuadrilatero
+    {
+        public Rectangulo(List<decimal> coord) : base(coord){}
+
+        public override decimal CalcularArea()
+        {
+            var baseRectangulo = base.Coordenadas[2] - base.Coordenadas[0]; // Coord x2 - x1
+            var alturaRectangulo = base.Coordenadas[5] - base.Coordenadas[1]; // Coord y3 - y1
+
+            return baseRectangulo * alturaRectangulo;
+        }
+    }
+}
--- EjercicioCuadrilateros/EjercicioCuadrilateros/Program.cs
+++ EjercicioCuadrilateros/EjercicioCuadrilateros/Program.cs
@@ -1,15 +1,33 @@
 ﻿using EjercicioCuadrilateros.Modelo;
 
-List<decimal> list = new List<decimal>();
-list.Add(0); // x1
-list.Add(0); // y1
-list.Add(7); // x2
-list.Add(0); // y2
-list.Add(2); // x3
-list.Add(4); // y3
-list.Add(5); // x4
-list.Add(4); // y4
+// coordenadas de la base de los cuadrilateros
+var x1 = 0;
+var y1 = 0;
+var x2 = 4;
+var y2 = 0;
 
-Trapecio t = new Trapecio(list);
+// coordenadas altura de cuadrado
+// se usan x1 y x2 para hc
+var hc = 4;
 
-Console.WriteLine(t.CalcularArea());
+// coordenadas altura de rectangulo
+// se usan x1 y x2 para hr
+var hr = 2;
+
+// coordenadas lado superior de trapecio
+var x3 = 1;
+var y3 = 2;
+var x4 = 3;
+var y4 = 2;
+
+List<decimal> listc = new List<decimal> { x1,y1,x2,y2,x1,hc,x2,hc};
+List<decimal> listr = new List<decimal> { x1, y1, x2, y2, x1, hr, x2, hr };
+List<decimal> listt = new List<decimal> { x1, y1, x2, y2, x3, y3, x4, y4 };
+
+Trapecio t = new Trapecio(listt);
+Cuadrado c = new Cuadrado(listc);
+Rectangulo r = new Rectangulo(listr);
+
+Console.WriteLine("El área del cuadrado de base = 4cm es: " + c.CalcularArea() + "cm2");
+Console.WriteLine("El área del rectangulo de base = 4cm y de altura igual a 2cm es: " + r.CalcularArea() + "cm2");
+Console.WriteLine("El área del trapecio de base inferior = 4cm y  de base superior = 2cm es: " + t.CalcularArea() + "cm2");
